3f85f8aa3202cd7a287980c3c754ae465e5f8eb1
osvaldsoza <pacificosuldev001@github.com>
2019-10-14 14:13:49 -0300

    atualizacao

--- src/App.js
+++ src/App.js
@@ -15,16 +15,17 @@ const generos = [
 
 const filmes = [
   { titulo: 'O Senhor dos Aneis', diretor: 'Peter jackson', quantidade: 4, genero: 'Aventura' },
-  { titulo: 'O Senhor dos Aneis', diretor: 'Peter jackson', quantidade: 4, genero: 'Aventura' },
-  { titulo: 'O Senhor dos Aneis', diretor: 'Peter jackson', quantidade: 4, genero: 'Aventura' },
-  { titulo: 'O Senhor dos Aneis', diretor: 'Peter jackson', quantidade: 4, genero: 'Aventura' }
+  { titulo: 'fgfgfgfg', diretor: 'Peter jackson', quantidade: 4, genero: 'Aventura' },
+  { titulo: 'çlçllçllçlçl', diretor: 'Peter jackson', quantidade: 4, genero: 'Aventura' },
+  { titulo: 'gflç90yutty', diretor: 'Peter jackson', quantidade: 4, genero: 'Aventura' }
 ]
 
 const initialState = {
   genero: '',
   titulo: '',
   diretor: '',
-  quantidade: ''
+  quantidade: '',
+  novoFilme: false
 }
 
 class App extends Component {
@@ -43,61 +44,88 @@ class App extends Component {
     console.log(this.state.genero, this.state.titulo, this.state.diretor)
   }
 
+  handleClickNovo = () => {
+    this.setState({ novoFilme: true });
+  }
+
   handleExcluir = () => {
     console.log('this.state.filmeSelected')
   }
 
   actionTemplate(rowData, column) {
     return <div className="d-flex">
-      <Button type="button" icon="pi pi-trash" className="p-button-secondary" style={{ marginRight: '2px' }} onClick={this.handleExcluir}></Button>
+      <Button type="button" icon="pi pi-trash" className="p-button-secondary" style={{ marginRight: '.5em' }} onClick={this.handleExcluir}></Button>
       <Button type="button" icon="pi pi-pencil" className="p-button-secondary"></Button>
     </div>;
   }
 
   handleRowSelect = (e) => {
-    this.setState({ filmeSelected:e.value });
+    this.setState({ filmeSelected: e.value });
   }
 
   render() {
     return (
       <div className='container'>
-
-        <h2 style={{ marginBottom: '20px' }}>Novo Filme</h2>
-        <span className="p-float-label" style={{ marginBottom: '20px' }}>
-          <InputText id="in" value={this.state.titulo} onChange={this.handleChangeField} name="titulo" className="p-inputtext" />
-          <label htmlFor="in">Título</label>
-        </span>
-        <span className="p-float-label" style={{ marginBottom: '20px' }}>
-          <InputText id="in" value={this.state.diretor} onChange={this.handleChangeField} name="diretor" className="p-inputtext" />
-          <label htmlFor="in">Diretor</label>
-        </span>
-        <span className="p-float-label" style={{ marginBottom: '20px' }}>
-          <InputText keyfilter="int" id="in" value={this.state.quantidade} onChange={this.handleChangeField} name="quantidade" className="p-inputtext" />
-          <label htmlFor="in">Quantidade</label>
-        </span>
-        <Dropdown
-          optionLabel="label"
-          value={this.state.genero}
-          options={generos}
-          onChange={this.handleChangeSelect}
-          placeholder="Generos"
-        />
-        <div>
-          <Button
-            style={{ marginTop: '10px' }}
-            label="Salvar"
-            icon="pi pi-check"
-            iconPos="right"
-            onClick={this.handleClickSalvar}
-          />
-        </div>
-
-        <DataTable header="Filmes" value={filmes} onSelectionChange={this.handleRowSelect} selectionMode="single" selection={this.state.filmeSelected}>
+        {
+          this.state.novoFilme ?
+<div>
+            <h2 style={{ marginBottom: '20px' }}>Novo Filme</h2>
+            <span className="p-float-label" style={{ marginBottom: '20px' }}>
+              <InputText id="in" value={this.state.titulo} onChange={this.handleChangeField} name="titulo" className="p-inputtext" />
+              <label htmlFor="in">Título</label>
+            </span>
+            <span className="p-float-label" style={{ marginBottom: '20px' }}>
+              <InputText id="in" value={this.state.diretor} onChange={this.handleChangeField} name="diretor" className="p-inputtext" />
+              <label htmlFor="in">Diretor</label>
+            </span>
+            <span className="p-float-label" style={{ marginBottom: '20px' }}>
+              <InputText keyfilter="int" id="in" value={this.state.quantidade} onChange={this.handleChangeField} name="quantidade" className="p-inputtext" />
+              <label htmlFor="in">Quantidade</label>
+            </span>
+            <Dropdown
+              optionLabel="label"
+              value={this.state.genero}
+              options={generos}
+              onChange={this.handleChangeSelect}
+              placeholder="Generos"
+            />
+            <div>
+              <Button
+                style={{ marginTop: '10px' }}
+                label="Salvar"
+                icon="pi pi-check"
+                iconPos="right"
+                onClick={this.handleClickSalvar}
+              />
+            </div>
+            <hr />
+            </div>
+          :
+            ''
+        }
+        <DataTable
+          header="Filmes"
+          paginator="true"
+          rows="10"
+          value={filmes}
+          selectionMode="single"
+          selection={this.state.filmeSelected}
+          onSelectionChange={e => this.setState({ filmeSelected: e.value })}
+        >
           <Column field="titulo" header="Título" />
           <Column field="diretor" header="Diretor" />
           <Column field="genero" header="Gênero" />
-          <Column body={this.actionTemplate} style={{ textAlign: 'center', width: '6em' }} />
+          <Column body={this.actionTemplate} style={{ textAlign: 'center', width: '7em' }} />
         </DataTable>
+
+        <Button
+          style={{ marginTop: '10px' }}
+          label="Novo Filme"
+          icon="pi pi-check"
+          iconPos="right"
+          hidden={this.state.novoFilme}
+          onClick={this.handleClickNovo}
+        />
       </div>
     );
 
